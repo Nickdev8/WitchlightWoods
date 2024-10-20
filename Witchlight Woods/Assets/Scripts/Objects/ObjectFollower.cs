e5c9c34ecf5277dd8d703f61c20831e3df471dd2
Marcin Smoła <Marcinure1@gmail.com>
2024-10-20 14:05:58 +0200

    Hair

---
+++ Witchlight Woods/Assets/Scripts/Objects/ObjectFollower.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace WitchlightWoods.Objects
+{
+    public class ObjectFollower : MonoBehaviour
+    {
+        public Transform target;
+        private bool _useRigidbody;
+        private Rigidbody2D _rigidbody2D;
+
+        private void Awake()
+        {
+            _useRigidbody = TryGetComponent(out _rigidbody2D);
+        }
+
+        private void Update()
+        {
+            if(!_useRigidbody)
+                transform.position = target.position;
+        }
+
+        private void FixedUpdate()
+        {
+            if(_useRigidbody)
+                _rigidbody2D.MovePosition(target.position);
+        }
+    }
+}
